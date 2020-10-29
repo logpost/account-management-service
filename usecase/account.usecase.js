cb8500eb26118785363ce9628b9eea502a7931f1
poonsht <poon_arsene_lupin@hotmail.com>
2020-10-29 20:21:43 +0700

    [+] fix : role instead account_type semantic on schema

--- usecase/account.usecase.js
+++ usecase/account.usecase.js
@@ -8,11 +8,11 @@ class AccountUsecase {
     
     fetcher = new AccountFactory()
 
-    signup = async (account_type, profile) => {
-        const { username, email, name } = profile
-        const res = await this.fetcher.account[account_type].createAccount(profile)
+    signup = async (role, profile) => {
+        const { username, email, name, account_type } = profile
+        const res = await this.fetcher.account[role].createAccount(profile)
         if(res.status === 200){ 
-            const payload = { username, email, name, account_type }
+            const payload = { username, email, name, role, account_type }
             const claims = { 
                 expiresIn: config.jwt.confirm_email.options.expires_in,
                 issuer: config.jwt.confirm_email.options.issuer,
@@ -21,15 +21,15 @@ class AccountUsecase {
             const secret = config.jwt.confirm_email.secret.jwt_secret
             const token_email = jwt.sign(payload, secret, claims)
             const transporter = await NodeMailerAdapter.getInstance()
-            await transporter.send(name, email, token_email)
+            await transporter.send(name, email, role, token_email)
             return { token_email, message: `Done, Message sent to ${email} success. Check your mailbox.` }
         
         }
         throw new Error(res.error.message)
     }
 
-    login = async (account_type, username, password) => { 
-        const res = await this.fetcher.account[account_type].findAccountByUsername(username)
+    login = async (role, username, password) => { 
+        const res = await this.fetcher.account[role].adminFindAccountByUsername(username)
         const { data: account } = res
         if(account){
             const authorized = await compareHashed(password, account.password)
@@ -39,7 +39,9 @@ class AccountUsecase {
                     name: account.name, 
                     display_name: account.display_name,
                     email: account.email, 
-                    account_type: account.account_type }
+                    account_type: account.account_type,
+                    role
+                 }
                 const claims = { 
                     expiresIn: config.jwt.private_route.options.expires_in,
                     issuer: config.jwt.private_route.options.issuer,
@@ -52,6 +54,16 @@ class AccountUsecase {
             throw new Error('400 : Invalid, password is not match')
         }
         throw new Error(res.error.message)
+    }
+    
+    adminFindAccountByUsername = async (role, username) => {
+        const res = await this.fetcher.account[role].adminFindAccountByUsername(username)
+        return res 
+    }
+
+    confirmedWithEmail = async (role, username, email) => {
+        const res = await this.fetcher.account[role].confirmedWithEmail(username, email)
+        return res 
     }
 
     createService = async (service_name) => {
