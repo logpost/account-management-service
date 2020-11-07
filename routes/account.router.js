83b45aa089899031483982589e246148bddf9788
poonsht <poon_arsene_lupin@hotmail.com>
2020-11-07 21:30:15 +0700

    [+] fix : move confirm on frontend to backend and redirect to frontend

--- routes/account.router.js
+++ routes/account.router.js
@@ -3,7 +3,8 @@ import express from 'express'
 import AccountUsecase from '../usecase/account.usecase'
 import NodeMailerAdapter from '../adapters/nodemailer.adapter'
 import passport from '../middlewares/auth.middleware'
-import responseHandler from '../helper/response.handler'
+import responseHandler, { responseSender } from '../helper/response.handler'
+
 
 const prefix = '/account'
 const router = express.Router()
@@ -21,7 +22,7 @@ router.get(`${prefix}/healthcheck/token/email`, passport.verifyEmail, async (req
     }, res)
 })
 
-router.get(`${prefix}/healthcheck/token/private`, passport.requireAuth, async (req, res) => {
+router.get(`${prefix}/healthcheck/token/private`, passport.verifyAuth, async (req, res) => {
     responseHandler(async () => {
         return { msg: `ok ${req.user}, verify token_private`}
     }, res)
@@ -30,9 +31,13 @@ router.get(`${prefix}/healthcheck/token/private`, passport.requireAuth, async (r
 router.post(`${prefix}/signup/:role`, async (req, res) => {
     responseHandler(async () => {
         const { role } = req.params
-        const profile = req.body 
-        const data =  await accountUsecase.signup(role, profile)
-        return data
+        const profile = req.body
+        if(role){
+            const data =  await accountUsecase.signup(role, profile)
+            return data
+        } else {
+            throw new Error('400 : Invalid, role is empty on param query')
+        }
     }, res)
 })
 
@@ -40,35 +45,41 @@ router.post(`${prefix}/login/:role`, async (req, res) => {
     responseHandler(async () => {
         const { role } = req.params
         const { username, password } = req.body 
-        const token = await accountUsecase.login(role, username, password)
-        return { token }
+        if(role){
+            const token = await accountUsecase.login(role, username, password)
+            return { token }
+        } else {
+            throw new Error('400 : Invalid, role is empty on param query')
+        }
     }, res)
 })
 
-router.post(`${prefix}/email/confirm/publish`, passport.verifyEmail, async (req, res) => {
+router.post(`${prefix}/email/confirm/send`, passport.verifyEmail, async (req, res) => {
     responseHandler(async () => {
-        const { isConfirmEmail, role, profile } = req.user
-        const { name, email } = profile
+        const { isConfirmEmail, role, name, email } = req.user
         const { token_email } = req.params
+        
         if(isConfirmEmail)
             return `200 : ${email} has been confirmed.`
+
         const transporter = await NodeMailerAdapter.getInstance()
         transporter.send(name, email, role, token_email)
         return `200 : Done, Message sent to ${email} success. Check your mailbox.`
-        
     }, res)
 })
 
-router.get(`${prefix}/email/confirm/consume`, passport.verifyEmail, async (req, res) => {
-    responseHandler(async () => {
-        const { isConfirmEmail, role, profile} = req.user
-        console.log(isConfirmEmail, role, profile)
-        const { email, username } = profile
-        if(isConfirmEmail)
-            return `200 : ${email} has been confirmed.`
-        const res = await accountUsecase.confirmedWithEmail(role, username, email)
-        return res.data.success.message
-    }, res)
+router.get(`${prefix}/email/confirm/recive/success`, passport.verifyEmail, async (req, res) => {
+        const { isConfirmEmail, role, email, username } = req.user    
+        if(isConfirmEmail) {
+            responseSender( `200 : ${email} has been confirmed.`, res)
+        }
+
+        try {
+            await accountUsecase.confirmedWithEmail(role, username, email)
+            res.redirect('https://google.com')    
+        } catch (error) {
+            responseSender(new Error(error), res)
+        }
 })
 
 router.get(`${prefix}/create/service/:service_name`, async (req, res) => {
@@ -76,7 +87,7 @@ router.get(`${prefix}/create/service/:service_name`, async (req, res) => {
         const { service_name } = req.params
         console.log(service_name)
         const token_service = await accountUsecase.createService(service_name)
-        return { token_service}
+        return { token_service }
     }, res)
 })
 
