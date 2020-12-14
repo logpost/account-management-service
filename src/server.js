9f46aa4e7496536559f5daa185a1b292bb9267e5
poonsht <poon_arsene_lupin@hotmail.com>
2020-12-14 22:17:12 +0700

    [+] add : port for running app on FaaS

--- src/server.js
+++ src/server.js
@@ -1,22 +1,24 @@
-import express from 'express'
-import cors from 'cors'
-import config from './config'
+import express from "express";
+import cors from "cors";
+import config from "./config";
 
-import NodeMailerAdapter from './adapters/nodemailer.adapter'
-import AccountRoute from './routes/account.router'
+import NodeMailerAdapter from "./adapters/nodemailer.adapter";
+import AccountRoute from "./routes/account.router";
 
-const app = express()
-const port = config.app.port
-const domain = config.app.domain
+const app = express();
+const port = process.env.PORT || config.app.port;
+const domain = config.app.domain;
 
-NodeMailerAdapter.getInstance()
+NodeMailerAdapter.getInstance();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/', AccountRoute)
+app.use("/", AccountRoute);
 
 app.listen(port, () => {
-console.log(`Account Management Service 👨🏼‍💻👩🏾‍💻\nApp listening on the http://${domain}:${port} 🌟`)
-})
+    console.log(
+        `TEST IMAGE => Account Management Service 👨🏼‍💻👩🏾‍💻\nApp listening on the http://${domain}:${port} 🌟`
+    );
+});
