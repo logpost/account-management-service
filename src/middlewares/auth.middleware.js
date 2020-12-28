c8fa60ce7872fe1718b1dfff323fd7329b2f21a7
poonsht <poon_arsene_lupin@hotmail.com>
2020-12-28 15:56:06 +0700

    [+] add : refresh_ token and rename email_token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,73 +1,95 @@
-import passport from 'passport'
-import { ExtractJwt, Strategy } from 'passport-jwt'
-import AccountUsecase from '../usecase/account.usecase'
-import config from '../config'
-import { checkAccountDidConfirmEmail } from '../helper/policy.handler'
+import passport from "passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import AccountUsecase from "../usecase/account.usecase";
+import config from "../config";
+// import { checkAccountDidConfirmEmail } from "../helper/policy.handler";
 
-const accountUsecase = new AccountUsecase()
+const accountUsecase = new AccountUsecase();
 
 const options_email_rule = {
-  jwtFromRequest: ExtractJwt.fromUrlQueryParameter("token_email"),
-  secretOrKey: config.jwt.confirm_email.secret.jwt_secret,
-  issuer: config.jwt.confirm_email.options.issuer,
-  audience: config.jwt.confirm_email.options.audience,
-}
+    jwtFromRequest: ExtractJwt.fromUrlQueryParameter("email_token"),
+    secretOrKey: config.jwt.email_token.secret.jwt_secret,
+    issuer: config.jwt.email_token.options.issuer,
+    audience: config.jwt.email_token.options.audience,
+};
 
 const options_auth_rule = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: config.jwt.private_route.secret.jwt_secret,
-  issuer: config.jwt.private_route.options.issuer,
-  audience: config.jwt.private_route.options.audience,
-}
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.jwt.access_token.secret.jwt_secret,
+    issuer: config.jwt.access_token.options.issuer,
+    audience: config.jwt.access_token.options.audience,
+};
 
-passport.use("email_rule", new Strategy(options_email_rule, async (payload, done) => {  
-  	try {
-		const { username, role } = payload
-		const { data: account } = await accountUsecase.adminFindAccountByUsername(role, username)
-		
-		if (account)
-			return done(null, payload)
+const options_refresh_rule = {
+    jwtFromRequest: ExtractJwt.fromBodyField("refresh_token"),
+    secretOrKey: config.jwt.refresh_token.secret.jwt_secret,
+    issuer: config.jwt.refresh_token.options.issuer,
+    audience: config.jwt.refresh_token.options.audience,
+};
 
-		// this case its not test yet !!!!
-		return done(null, false)
-		
-	} catch (error) {
-		return done(error, false)
-	}
-}))
+passport.use(
+    "email_rule",
+    new Strategy(options_email_rule, async (payload, done) => {
+        try {
+            const { username, role } = payload;
+            const { data: account } = await accountUsecase.adminFindAccountByUsername(role, username);
 
-passport.use("auth_rule", new Strategy(options_auth_rule, async (payload, done) => {  
-    try {
-		const { username, role } = payload
-		const account = await accountUsecase.adminFindAccountByUsername(role, username)
-		
-		if (account) {
-			const user = {
-				isConfirmEmail: false,
-				shipper_id: account._id,
-				username: account.username,
-				name: account.name,
-				display_name: account.display_name,
-				email: account.email,
-				account_type: account.account_type,
-				role
-			}
+            if (account) return done(null, payload);
+            // this case its not test yet !!!!
+            return done(null, false);
+        } catch (error) {
+            return done(error, false);
+        }
+    })
+);
 
-		if(account.email !== 'not_confirm')
-			user.isConfirmEmail = true
+passport.use(
+    "auth_rule",
+    new Strategy(options_auth_rule, async (payload, done) => {
+        try {
+            const { username, role } = payload;
+            const { data: account } = await accountUsecase.adminFindAccountByUsername(role, username);
 
-		return done(null, user)
+            if (account) {
+                const user = {
+                    isConfirmEmail: false,
+                    shipper_id: account._id,
+                    username: account.username,
+                    name: account.name,
+                    display_name: account.display_name,
+                    email: account.email,
+                    account_type: account.account_type,
+                    role,
+                };
 
-		} else {
-		return done(null, false)
-		}
+                if (account.email !== "not_confirm") user.isConfirmEmail = true;
 
-	} catch (error) {
-		return done(error, false)
-	}
-}))
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        } catch (error) {
+            return done(error, false);
+        }
+    })
+);
+
+passport.use(
+    "refresh_rule",
+    new Strategy(options_refresh_rule, async (payload, done) => {
+        try {
+            const { username, role } = payload;
+            const { data: account } = await accountUsecase.adminFindAccountByUsername(role, username);
+            if (account) return done(null, account);
+            return done(null, false);
+        } catch (error) {
+            return done(error, false);
+        }
+    })
+);
 
 export default {
-  verifyAuth: passport.authenticate('auth_rule', { session: false }),
-  verifyEmail: passport.authenticate('email_rule', { session: false })
-}
+    verifyAuth: passport.authenticate("auth_rule", { session: false }),
+    verifyEmail: passport.authenticate("email_rule", { session: false }),
+    verifyRefreshToken: passport.authenticate("refresh_rule", { session: false }),
+};
