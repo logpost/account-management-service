cae9a578d14b17ae859f65e374567522a3de3b89
poonsht <poon_arsene_lupin@hotmail.com>
2020-12-28 15:56:25 +0700

    [+] fix : indents

--- src/adapters/nodemailer.adapter.js
+++ src/adapters/nodemailer.adapter.js
@@ -1,19 +1,17 @@
-import nodemailer from 'nodemailer'
-import config from '../config'
-import templateEmail from '../template/email.template'
+import nodemailer from "nodemailer";
+import config from "../config";
+import templateEmail from "../template/email.template";
 
 class NodeMailerAdapter {
-    
-    static instance = null
-    
-    #transporter = null
-    #transport_info = null
+    static instance = null;
+
+    #transporter = null;
+    #transport_info = null;
 
     #account = {
         user: config.nodemailer.auth.gmail.username,
         pass: config.nodemailer.auth.gmail.password,
-
-    }
+    };
 
     #mailpayload = {
         from: '"Logpost platform รวมงานขนส่ง 👻" <logpost.bot@gmail.com>', // sender address
@@ -21,15 +19,15 @@ class NodeMailerAdapter {
         subject: "ยืนยันอีเมลของคุณ ✔", // Subject line
         // text: "Hello world?", // plain text body
         html: "<b>Hello world?</b>", // html body
+    };
+
+    constructor() {
+        this.connect();
     }
 
-    constructor(){
-        this.connect()
-    }
-
-    static getInstance(){
-        if(!NodeMailerAdapter.instance) NodeMailerAdapter.instance = new NodeMailerAdapter()
-        return NodeMailerAdapter.instance
+    static getInstance() {
+        if (!NodeMailerAdapter.instance) NodeMailerAdapter.instance = new NodeMailerAdapter();
+        return NodeMailerAdapter.instance;
     }
 
     connect = async () => {
@@ -37,29 +35,38 @@ class NodeMailerAdapter {
             host: "smtp.ethereal.email",
             port: 587,
             secure: false, // true for 465, false for other ports
-            service: 'gmail',
+            service: "gmail",
             auth: {
-              user: this.#account.user, // generated ethereal user
-              pass: this.#account.pass, // generated ethereal password
-            }
-        }
+                user: this.#account.user, // generated ethereal user
+                pass: this.#account.pass, // generated ethereal password
+            },
+        };
 
-        this.#transporter = nodemailer.createTransport(this.#transport_info)
-        console.log("NodeMailer: transporter is ready ✉️")
-    }
+        this.#transporter = nodemailer.createTransport(this.#transport_info);
+        console.log("NodeMailer: transporter is ready ✉️");
+    };
 
-    send = async (name, email, role, token_email) => {
-        let info = await this.#transporter.sendMail({ ...this.#mailpayload, to: email, html: templateEmail(name, email, role, `http://${config.app.domain}:${config.app.port}/account/email/confirm/recive/success?token_email=${token_email}`) })
+    send = async (name, email, role, email_token) => {
+        let info = await this.#transporter.sendMail({
+            ...this.#mailpayload,
+            to: email,
+            html: templateEmail(
+                name,
+                email,
+                role,
+                `http://${config.app.domain}:${config.app.port}/account/email/confirm/recive/success?email_token=${email_token}`
+            ),
+        });
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        this.logging(info)
-    }
+        this.logging(info);
+    };
 
     logging = (info) => {
-        console.log("Message sent: %s", info.messageId)
+        console.log("Message sent: %s", info.messageId);
         // Preview only available when sending through an Ethereal account
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    }
+    };
 }
 
-export default NodeMailerAdapter
+export default NodeMailerAdapter;
