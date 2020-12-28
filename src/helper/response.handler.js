e17b8f0840ff3301b771141c59abff7932d3a2ab
poonsht <poon_arsene_lupin@hotmail.com>
2020-12-28 23:13:48 +0700

    [+] fix : indents

--- src/helper/response.handler.js
+++ src/helper/response.handler.js
@@ -1,18 +1,19 @@
-import errorHandler from './errors.handler'
-import parseResponse from './response.parser'
+import errorHandler from "./errors.handler";
+import parseResponse from "./response.parser";
 
 export const responseSender = async (data, res) => {
-  res = await errorHandler.response(parseResponse(data), res)
-  res.send(data)
-}
+    const dataParsed = parseResponse(data);
+    res = await errorHandler.response(dataParsed, res);
+    res.send(dataParsed);
+};
 
 const responseHandler = async (next, res) => {
-  try {
-    const data = await next()
-    responseSender(data, res)
-  } catch (error) {
-    responseSender(error, res)
-  }
-}
+    try {
+        const data = await next();
+        responseSender(data, res);
+    } catch (error) {
+        responseSender(error, res);
+    }
+};
 
-export default responseHandler
+export default responseHandler;
