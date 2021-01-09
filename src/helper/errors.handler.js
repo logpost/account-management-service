cf542729d69437cb9239f41983dd5c3302604dc2
poonsht <poon_arsene_lupin@hotmail.com>
2021-01-09 17:04:54 +0700

    [+] add : status 401 error code on error-handler

--- src/helper/errors.handler.js
+++ src/helper/errors.handler.js
@@ -1,26 +1,36 @@
 const response = (responseData, res) => {
-  let responseCode = res.header('Content-Type', 'application/json;charset=utf-8').status(200)
+    let responseCode = res.header("Content-Type", "application/json;charset=utf-8").status(200);
 
-  if ('error' in responseData) {
-    switch (responseData.error.code) {
-      case 400: {
-        responseCode = res.header('Content-Type', 'application/json;charset=utf-8').status(responseData.error.code)
-        break
-      }
-      case 403: {
-        responseCode = res.header('Content-Type', 'application/json;charset=utf-8').status(responseData.error.code)
-        break
-      }
-      case 404: {
-        responseCode = res.header('Content-Type', 'application/json;charset=utf-8').status(responseData.error.code)
-        break
-      }
-      default: {
-        responseCode = res.header('Content-Type', 'application/json;charset=utf-8').status(500)
-        break
-      }
+    if ("error" in responseData) {
+        switch (responseData.error.code) {
+            case 400: {
+                responseCode = res
+                    .header("Content-Type", "application/json;charset=utf-8")
+                    .status(responseData.error.code);
+                break;
+            }
+            case 401: {
+                reply.header("Content-Type", "application/json;charset=utf-8").code(replyponseData.error.code);
+                break;
+            }
+            case 403: {
+                responseCode = res
+                    .header("Content-Type", "application/json;charset=utf-8")
+                    .status(responseData.error.code);
+                break;
+            }
+            case 404: {
+                responseCode = res
+                    .header("Content-Type", "application/json;charset=utf-8")
+                    .status(responseData.error.code);
+                break;
+            }
+            default: {
+                responseCode = res.header("Content-Type", "application/json;charset=utf-8").status(500);
+                break;
+            }
+        }
     }
-  }
-  return responseCode
-}
-export default { response }
+    return responseCode;
+};
+export default { response };
